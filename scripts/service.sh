bd9ce54df06cdda5341a0ec6a551b365eb00f2d4
BobbyKataria <bobby.kataria@hotmail.co.uk>
2019-08-07 14:09:52 +0000

    testing

---
+++ scripts/service.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+sudo apt -y upgrade
+sudo apt install maven
+#mvn clean package
+sudo useradd serviceadmin 
+
+sudo chown -R serviceadmin /etc/systemd/system
+sudo chown -R serviceadmin ~/QA-Portal/qa-portal-services/
+sudo chown -R serviceadmin /usr/bin/java
+
+#If the filename does have api in the title please insert in VAR, if not insert below into VAR2
+
+VAR=("core-api" "user-api")
+
+for val2 in ${VAR[*]}; do
+	sudo mkdir /opt/$val2
+	sudo cp ~/QA-Portal/qa-portal-services/$val2/target/$val2-0.0.1-SNAPSHOT.jar /opt/$val2/
+	sudo chown -R serviceadmin /opt/$val2
+	sudo systemctl daemon-reload
+done
+
+for val in ${VAR[*]}; do
+	sudo systemctl stop $val
+	sudo sed "s/{{NAME}}/$val/g" template.service | sudo tee /etc/systemd/system/$val.service 
+	sudo systemctl start $val
+	sudo systemctl daemon-reload
+done
+
+VAR2=("self-reflection")
+
+for val3 in ${VAR2[*]}; do
+        sudo mkdir /opt/$val3
+        sudo cp ~/QA-Portal/qa-portal-services/$val3/target/$val3-api-0.0.1-SNAPSHOT.jar /opt/$val3/
+        sudo chown -R serviceadmin /opt/$val3
+	sudo systemctl daemon-reload
+done
+
+for val3 in ${VAR2[*]}; do
+        sudo systemctl stop $val3
+        sudo sed "s/{{NAME}}/$val3/g" template-api.service | sudo tee /etc/systemd/system/$val3.service
+        sudo systemctl start $val3
+done
+
+VAR3=("self-reflection" "user-api" "core-api")
+
+
+for value in ${VAR3[*]}; do
+	sudo systemctl daemon-reload
+	sudo systemctl stop $value
+	sudo systemctl start $value
+done
